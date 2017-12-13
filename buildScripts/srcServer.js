9b3cdc86eb22ce9c9b95037ed8f64e1fa28dc9dd
RSXII <ryan@ryandschultz.com>
2017-12-13 12:42:44 -0700

    Initial Build Update

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -6,13 +6,13 @@ const port = 3000;
 const app = express();
 
 app.get('/', function(req,res){
-    res.sendFile(path.join(__dirname, '../src/index.html'))
+    res.sendFile(path.join(__dirname, '../src/index.html'));
 });
 
 app.listen(port, function(err){
     if(err){
         console.log(err);
     }else{
-        open()
+        open('http://localhost:' + port);
     }
-})
+});
